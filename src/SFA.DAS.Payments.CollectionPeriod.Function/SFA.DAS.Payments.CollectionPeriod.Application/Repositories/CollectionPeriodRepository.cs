f8cd7ed61c86866cf65f4881195c60a3d91d0336
syed-wali <6061008+syed-wali@users.noreply.github.com>
2026-02-02 16:05:04 +0000

    PV2-3855 Added Collection Period Repository and DI for PaymentsDbContext

---
+++ src/SFA.DAS.Payments.CollectionPeriod.Function/SFA.DAS.Payments.CollectionPeriod.Application/Repositories/CollectionPeriodRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SFA.DAS.Payments.Application.Repositories;
+using SFA.DAS.Payments.Model.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Payments.CollectionPeriod.Application.Repositories
+{
+    public interface ICollectionPeriodRepository
+    {
+        Task<IEnumerable<CollectionPeriodModel>> GetCollectionPeriodByAcademicYear(short academicYear);
+    }
+
+    public class CollectionPeriodRepository : ICollectionPeriodRepository
+    {
+        private readonly IPaymentsDataContext _paymentsDataContext;
+        public CollectionPeriodRepository(IPaymentsDataContext paymentsDataContext)
+        {
+            _paymentsDataContext = paymentsDataContext;
+        }
+
+        public async Task<IEnumerable<CollectionPeriodModel>> GetCollectionPeriodByAcademicYear(short academicYear)
+        {
+            return await _paymentsDataContext.CollectionPeriod.Where(cp => cp.AcademicYear == academicYear)
+                .ToListAsync();
+        }
+    }
+}
--- src/SFA.DAS.Payments.CollectionPeriod.Function/SFA.DAS.Payments.CollectionPeriod.Function/CollectionPeriodsByAcademicYear.cs
+++ src/SFA.DAS.Payments.CollectionPeriod.Function/SFA.DAS.Payments.CollectionPeriod.Function/CollectionPeriodsByAcademicYear.cs
@@ -2,22 +2,32 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using SFA.DAS.Payments.Application.Repositories;
+using SFA.DAS.Payments.CollectionPeriod.Application.Repositories;
 
 namespace SFA.DAS.Payments.CollectionPeriod.Function;
 
 public class CollectionPeriodsByAcademicYear
 {
     private readonly ILogger<CollectionPeriodsByAcademicYear> _logger;
+    private readonly ICollectionPeriodRepository _collectionPeriodRepository;
 
-    public CollectionPeriodsByAcademicYear(ILogger<CollectionPeriodsByAcademicYear> logger)
+    public CollectionPeriodsByAcademicYear(ILogger<CollectionPeriodsByAcademicYear> logger, ICollectionPeriodRepository collectionPeriodRepository)
     {
         _logger = logger;
+        _collectionPeriodRepository = collectionPeriodRepository;
     }
 
-    [Function("Function1")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    [Function("CollectionPeriodsByAcademicYear")]
+    public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!");
+        short.TryParse(req.Query["academicYear"], out var academicYear);
+
+        _logger.LogInformation("CollectionPeriodsByAcademicYear HTTP trigger function for academic year: {academicYear}.", academicYear);
+
+        var collectionPeriods = await _collectionPeriodRepository.GetCollectionPeriodByAcademicYear(academicYear);
+
+        _logger.LogInformation("CollectionPeriodsByAcademicYear processed for academic year: {academicYear}.", academicYear);
+        return new OkObjectResult(collectionPeriods);
     }
 }
