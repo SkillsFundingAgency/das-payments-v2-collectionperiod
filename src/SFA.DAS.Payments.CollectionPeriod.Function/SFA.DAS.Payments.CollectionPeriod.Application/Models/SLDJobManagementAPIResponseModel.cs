3b2689a94a9edb1dd215631bc24d35747f55277e
syed-wali <6061008+syed-wali@users.noreply.github.com>
2026-02-06 15:27:01 +0000

    PV2-3857 update SLDJobManagementAPICollectionPeriods model to include only required fields

--- src/SFA.DAS.Payments.CollectionPeriod.Function/SFA.DAS.Payments.CollectionPeriod.Application/Models/SLDJobManagementAPIResponseModel.cs
+++ src/SFA.DAS.Payments.CollectionPeriod.Function/SFA.DAS.Payments.CollectionPeriod.Application/Models/SLDJobManagementAPIResponseModel.cs
@@ -13,14 +13,7 @@ namespace SFA.DAS.Payments.CollectionPeriod.Application.Models
 
     public class SLDJobManagementAPICollectionPeriods
     {
-        public int ReturnPeriodId { get; set; }
-        public DateTime StartDateTimeUtc { get; set; }
-        public DateTime EndDateTimeUtc { get; set; }
         public byte PeriodNumber { get; set; }
-        public string CollectionName { get; set; }
-        public int CalendarMonth { get; set; }
-        public int CalendarYear { get; set; }
-        public int CollectionId { get; set; }
         public short CollectionYear { get; set; }
         public bool IsOpen { get; set; }
     }
