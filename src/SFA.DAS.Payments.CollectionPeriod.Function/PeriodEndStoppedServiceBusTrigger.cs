a62c28333685d98230a444c382f18a78deae1e36
Liam Wright <28269309+LiamWright@users.noreply.github.com>
2026-06-09 16:29:46 +0100

    PV2-4003 - Changed quene name variable for PeriodEndStoppedServiceBusTrigger

--- src/SFA.DAS.Payments.CollectionPeriod.Function/PeriodEndStoppedServiceBusTrigger.cs
+++ src/SFA.DAS.Payments.CollectionPeriod.Function/PeriodEndStoppedServiceBusTrigger.cs
@@ -23,7 +23,7 @@ public class PeriodEndStoppedServiceBusTrigger
 
     [Function(nameof(PeriodEndStoppedServiceBusTrigger))]
     public async Task Run(
-        [ServiceBusTrigger("%QueueName%", Connection = "%ServiceBusConnectionString%")]
+        [ServiceBusTrigger("%CollectionPeriodQueueName%", Connection = "%ServiceBusConnectionString%")]
         ServiceBusReceivedMessage message, ServiceBusMessageActions messageActions)
     {
         _logger.LogInformation("PeriodEndStoppedServiceBusTrigger function executed at: {executionTime}", DateTime.Now);
